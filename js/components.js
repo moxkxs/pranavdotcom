ab1651658ce5c5b2de173035e75293a499729667
moxkxs <pby5ws@virginia.edu>
2026-03-06 14:34:11 -0500

    updated twitter

--- js/components.js
+++ js/components.js
@@ -23,7 +23,7 @@
     ];
 
     var socials = [
-        { label: 'Twitter',  href: 'https://x.com/pbkandlakunta?lang=en',
+        { label: 'Twitter',  href: 'https://x.com/moxkxs?lang=en',
           d: 'M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z' },
         { label: 'LinkedIn', href: 'https://linkedin.com/in/pranavkandlakunta',
           d: 'M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z',
