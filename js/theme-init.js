398548f5987aa9ba8cd6dcd3c0a3323b36011c09
moxkxs <pby5ws@virginia.edu>
2026-03-11 16:18:22 -0400

    remove old root-level files superseded by src/

--- js/theme-init.js
+++
@@ -1,7 +0,0 @@
-/* theme-init.js — load before body renders to prevent flash */
-(function () {
-    try {
-        var t = localStorage.getItem('theme');
-        if (t) document.documentElement.setAttribute('data-theme', t);
-    } catch(e) {}
-})();
